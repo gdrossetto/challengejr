40b97b88efcb51486166501fa0c35282f3f993e2
gdrossetto <gabriel.dadamosrossetto@gmail.com>
2020-04-19 21:56:03 -0300

    teste auto-deploy heroku

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,15 @@
     <v-app-bar fixed dense style="background-color:#37125c;position:absolute">
       <v-row>
         <v-col>
-          <v-app-bar-nav-icon style="color:white" @click="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-app-bar-nav-icon
+            style="color:white"
+            @click="drawer = !drawer"
+          ></v-app-bar-nav-icon>
         </v-col>
         <v-col>
-          <v-toolbar-title class="titulo-appbar">Blog</v-toolbar-title>
+          <v-toolbar-title class="titulo-appbar"
+            >Blog Challenge</v-toolbar-title
+          >
         </v-col>
         <v-col></v-col>
       </v-row>
@@ -14,13 +19,20 @@
 
     <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list dense>
-        <v-list-item v-for="item in items" :key="item.title" link :to="item.path">
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="item.path"
+        >
           <v-list-item-icon style="color:#37125c">
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title style="color:#37125c">{{ item.title }}</v-list-item-title>
+            <v-list-item-title style="color:#37125c">{{
+              item.title
+            }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -46,16 +58,16 @@ export default {
         {
           title: "Categorias ",
           icon: "mdi-format-list-bulleted-type",
-          path: "/categorias"
+          path: "/categorias",
         },
         {
           title: "Criar Post ",
           icon: "mdi-newspaper-plus",
-          path: "/criar_post"
-        }
-      ]
+          path: "/criar_post",
+        },
+      ],
     };
-  }
+  },
 };
 </script>
 
